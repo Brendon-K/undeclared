893f185aac769fa7cf1e5484cc70574167aaf3b8
Brendon Kieser <brendon.kieser@gmail.com>
2022-02-10 12:58:52 -0800

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-async function read_nonsense() {
-  const url1 = 'https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_guesses.txt'
-  const response = await fetch(url1);
+async function read_txt_webpage(url) {
+  const response = await fetch(url);
   const data = await response.text();
   console.log(data);
 }
 
-const guesses_promise = read_nonsense();
+let guesses_url = "https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_guesses.txt"
+const guesses_promise = read_txt_webpage(guesses_url);
+
+let answers_url = "https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_answers.txt"
+const answers_promise = read_txt_webpage(answers_url);
 
 console.log("waiting for promise")
-$.when(guesses_promise).done(function{
+$.when(guesses_promise, answers_promise).done(function{
   console.log("promise received");
 });
