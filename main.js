3aeb915fcb4f3ae9cfcabde68206dbdcd8044634
Brendon Kieser <brendon.kieser@gmail.com>
2022-02-10 11:50:42 -0800

    testing

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const url1 = 'https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_guesses.txt'
-const response = await fetch(url1);
-const data = await response.text();
-console.log(data);
+async function read_nonsense() {
+  const url1 = 'https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_guesses.txt'
+  const response = await fetch(url1);
+  const data = await response.text();
+  console.log(data);
+}
+
+read_nonsense();
