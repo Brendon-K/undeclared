ef5b5f0bb01b911cbc84982d08784a1964d9cc4e
Brendon Kieser <brendon.kieser@gmail.com>
2022-02-10 16:33:51 -0800

    testing

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ async function read_txt_webpage(url) {
   console.log(data);
 }
 
+function make_guess(guess_num, word) {
+  $("#guess" + guess_num).children().each(function(index){
+    $(this).html(word[index]);
+  });
+}
+
 let guesses_url = "https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_guesses.txt"
 const guesses_promise = read_txt_webpage(guesses_url);
 
 let answers_url = "https://raw.githubusercontent.com/Brendon-K/undeclared/main/wordle_answers.txt"
 const answers_promise = read_txt_webpage(answers_url);
 
-console.log("waiting for promise")
 $.when(guesses_promise, answers_promise).done(function(){
-  console.log("promise received");
+  let bad_letters = [];
+  // guess soare first
+  make_guess(1, "soare");
 });
